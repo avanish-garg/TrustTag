a22b3da0344a33dbe6c48adadb6689d56c4bd2c9
Aastha-Rathi <aastharathi0404gmail.com>
2025-02-27 20:38:50 +0530

    containts register, login and getProfile

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+dotenv.config();
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/user", require("./routes/userRoutes"));
+
+// Default Route
+app.get("/", (req, res) => {
+  res.send("Decentralized Resume Verification API is running...");
+});
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
